0a6b88c94307a3aa47530ebe2c379d13735b9cdf
afsonk <q8737501@gmail.com>
2021-05-20 11:16:32 +0300

    ended up with sorting area

--- src/components/Home/Sort.tsx
+++ src/components/Home/Sort.tsx
@@ -1,19 +1,38 @@
+import classnames from "classnames"
+import {useState} from "react"
+import SortArrow from "./SortArrow"
 
 
 function Sort() {
     const sort = ['popularity', 'price', 'alphabet']
+    const [activeSort, setActiveSort] = useState(sort[0])
+    const [isVisible, setIsVisible] = useState(false)
+
+
+    const handleSortClick = (item: string): void => {
+        setActiveSort(item)
+        handleSortLabelClick()
+    }
+    const handleSortLabelClick = ():void => {
+        setIsVisible(prevState => !prevState)
+    }
+
+
     return (
         <div className={'sort'}>
-            <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z" fill="#2C2C2C"></path>
-            </svg>
+            <SortArrow isVisible={isVisible}/>
             <b>Sort by:</b>
-            <span>{sort[0]}</span>
-            <div className={'sort__popup'}>
+            <span className={'sort__label'} onClick={handleSortLabelClick}>{activeSort}</span>
+            <div className={classnames('sort__popup',{
+                'active': isVisible
+            })}>
                 <ul>
                     {
                         sort.map(item => {
-                            return <li>{item}</li>
+                            return <li className={classnames({
+                                'active': item === activeSort
+                            })}
+                            onClick={() => handleSortClick(item)}>{item}</li>
                         })
                     }
                 </ul>
