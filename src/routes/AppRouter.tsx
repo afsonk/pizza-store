fe11e14e95798e284a32b3de6ba3fa246ef1a7d7
Pasha <q8737501@gmail.com>
2022-06-30 21:36:41 +0300

    added lazy loading to heavy routes

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,14 +1,42 @@
 import { Route, Routes } from 'react-router-dom'
-import { Cart, CartEmpty, Checkout, CheckoutResult, Home } from '../pages'
+import { lazy, Suspense } from 'react'
+import { Home } from '../pages'
+
+const CheckoutPage = lazy(() => import('../pages/Checkout/Checkout'))
+const CheckoutResultPage = lazy(() => import('../pages/CheckoutResult/CheckoutResult'))
+const Cart = lazy(() => import('../pages/Cart/Cart'))
 
 function AppRouter() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/cart' element={<Cart />} />
-      <Route path='/cartEmpty' element={<CartEmpty />} />
-      <Route path='/checkout' element={<Checkout />} />
-      <Route path='/checkout/result' element={<CheckoutResult />} />
+
+      <Route
+        path='/cart'
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Cart />
+          </Suspense>
+        }
+      />
+
+      <Route
+        path='/checkout'
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <CheckoutPage />
+          </Suspense>
+        }
+      />
+
+      <Route
+        path='/checkout/result'
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <CheckoutResultPage />
+          </Suspense>
+        }
+      />
     </Routes>
   )
 }
